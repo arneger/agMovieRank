bc7c26f861abf64bcfd9909df129db26c437c066
agrekkedal <agrekkedal1@gmail.com>
2020-02-07 14:54:56 +0100

    Changed some function and attribute names. Also tried to fix the strange load animation that happens when the internet is slow.

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function changePosterStyle(noCatch){
 }
 
 // Finds the movie information from the API and inserts it to the HTML.
-function apiCall(){
+function callApiInfo(){
     movie = getInput();
     nocatch = true;
     const postsPromise = fetch('https://www.omdbapi.com/?apikey=ea9f22bf&t=' + encodeURI(movie));
@@ -41,8 +41,9 @@ function apiCall(){
         var averageRating = Math.round((imdbRating+rTomatoesRating+mCriticRating)/3);
         var scoreOutput = averageRating.toString() + "/100";
         var moviePoster = data["Poster"]
-        document.getElementById("someText").innerHTML = data["Title"];
+        document.getElementById("theMovieTitle").innerHTML = data["Title"];
         document.getElementById("rating").innerHTML = scoreOutput;
+        document.getElementById("poster").src = null;
         styleReset();
         changePosterStyle(nocatch);
         document.getElementById("poster").src = moviePoster;
@@ -50,8 +51,9 @@ function apiCall(){
         }
         catch(err){
             nocatch = false;
-            document.getElementById("someText").innerHTML = "";
+            document.getElementById("theMovieTitle").innerHTML = "";
             document.getElementById("rating").innerHTML = "Could not find " +'"'+ movie + '"'+".<br/>Try typing the correct movie title.";
+            document.getElementById("poster").src = null;
             styleReset();
             changePosterStyle(nocatch);
             document.getElementById("poster").src = "errorImage2.png";
@@ -62,11 +64,11 @@ function apiCall(){
     })
 }
 
-// Make it so the user can press enter instead of using the button to call the apiCall.
+// Make it so the user can press enter instead of using the button to call the callApiInfo.
 let input = document.querySelector('input');
 input.addEventListener('keyup', (e) => {
     if(e.keyCode === 13) {
-        apiCall();
+        callApiInfo();
     }
 })
 
